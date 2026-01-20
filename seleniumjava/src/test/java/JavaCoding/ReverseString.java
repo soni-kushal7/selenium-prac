f1ab5772b140eb4b198fe8684eca08acbf0fcd5c
soni-kushal7 <soni.kushal112@gmail.com>
2026-01-20 18:51:19 +0530

    ReverseString Program

--- seleniumjava/src/test/java/JavaCoding/ReverseString.java
+++ seleniumjava/src/test/java/JavaCoding/ReverseString.java
@@ -20,62 +20,24 @@ public class ReverseString {
 		
 		
 		
-//		for (int i=OrignalStr.length() -1; i>=0; i-- ) {
-//			
-//			revString= revString + OrignalStr.charAt(i);
-//			
-//			
-//		}
-//		
-//		System.out.println("Orignal String is : "+ OrignalStr);
-//		System.out.println("Reverse String is : "+ revString);
-//		
-//
-//	}
+
 		
-		
-		
-//		String str = "kushal verma";
-//		
-//		String rev = "";
-//		
-//		for (int i=str.length()- 1;i >=0 ; i--) {
-//			
-//			
-//			rev=rev+ str.charAt(i);
-//		
-//		
-//		}
-//		
-//		System.out.println("Origenal String is : " + str);
-//		
-//		System.out.println("Reverse String is : " + rev);
 	
+	String str = "Kushal";
+	String rev= "";
+	
+	for (int i=str.length()-1; i>=0 ; i--) {
 		
+		rev= rev+ str.charAt(i);
 		
-		String originalStr = "Learn Java Programing";
-        String[] words = originalStr.split(" ");
-        String revSentence = "";
-
-        for (String word : words) {
-
-            String revWord = "";
-
-            for (int i = word.length() - 1; i >= 0; i--) {
-                revWord = revWord + word.charAt(i);
-            }
-
-            revSentence = revSentence + revWord + " ";
-        }
-
-        System.out.println("Original String : " + originalStr);
-        System.out.println("Reversed String : " + revSentence.trim());
-		
-		
-        
-        
-        
-        
+	}
+	
+	
+	System.out.println(rev);
+	
+	
+	
+	    
 		
 		
 	}
