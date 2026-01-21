70ce812dfaabb4a2b5befb51138cccb6499726f5
soni-kushal7 <soni.kushal112@gmail.com>
2026-01-21 16:40:48 +0530

    MethodOverloading and MethodOverriding

---
+++ seleniumjava/src/test/java/Polymorphism/Human.java
@@ -0,0 +1,41 @@
+package Polymorphism;
+
+ 
+class Human {
+	
+	void role() {
+		
+		System.out.println("I am Human");
+		
+	}
+
+}
+
+
+
+ class Husband extends Human {
+	
+	void role() {
+		
+		System.out.println("I am husband");
+		
+		
+	}
+
+}
+ 
+ 
+ 
+ class Bro extends Human{
+		
+		void role() {
+			
+			System.out.println("I am brother");
+			
+		}
+				
+	}
+ 
+
+ 
+ 
